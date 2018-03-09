8da196ab18018bcc197b32782948ac7bcced0e99
toanmx0101 <toanmx.01@gmail.com>
2018-03-09 08:46:38 +0700

    Add 2 layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  layout  :application_layout
   private
 
   def account_update_params
     params.require(:user).permit(:email, :password, :password_confirmation, :avatar)
+  end
+
+  def application_layout
+  	user_signed_in? ? "application" : "signed_in_application"
   end
 end
